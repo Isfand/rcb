587612192dd82c6443d2e8926dd11d363261b5f5
Isfand <Isfand@users.noreply.github.com>
2024-11-12 22:49:54 +0000

    Added try catch inside Validate::wipe()

--- trash/validate.cxx
+++ trash/validate.cxx
@@ -172,10 +172,26 @@ void Validate::wipe()
 		if(confirmFlag || m_vOpt.forceOption || m_vOpt.silentOption)
 		{
 			for (const auto& entry : std::filesystem::directory_iterator(singleton->getWorkingTrashWipeDir()))
-				std::filesystem::remove_all(entry.path());
+			{
+				try
+				{
+					std::filesystem::remove_all(entry.path());
+				}
+				catch (std::runtime_error& e) 
+				{
+					/* NOTE: Exception likely happens due to a permissions issue as remove_all recursively removes files, NOT directories, individually.
+					It will only remove a directory when it's empty, otherwise it will continue to remove all contents inside.
+					E.G If a directory has another directory that has root ownership with contents inside, they cannot be removed. 
+					This is because the program is executed by a user that is missing the required permissions for the nested directory, but has permissions to remove contents in the top-level directory.
+
+					The exception is then thrown. Executing the program as root allows the operation to finish without throwing any exceptions.
+					*/
+					if(!m_vOpt.silentOption) std::cerr << e.what() << "\n";
+					continue;
+				}
+			}
 		} 
 	}
-
 } 
 
 void Validate::fillDirectorySize()
