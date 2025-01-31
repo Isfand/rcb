3d56251925de23c977fc604a6505506f980dd517
Isfand <Isfand@users.noreply.github.com>
2025-01-31 00:35:12 +0000

    Updated filename

--- config_init.ps1
+++ config_init.ps1
@@ -6,18 +6,18 @@ if ($ARCH -eq "AMD64" -or $ARCH -eq "x86")
 {
 	& {
 		Write-Output "x86 detected" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @x64-win32-msvc-debug cc config.config.load=tools/build_configs/windows-msvc-debug.config" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @x64-win32-llvm-debug cc config.config.load=tools/build_configs/windows-llvm-debug.config" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @x64-win32-llvm-release cc config.config.load=tools/build_configs/windows-llvm-release.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @x64-win32-msvc-debug cc config.config.load=tools/build-configs/windows-msvc-debug.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @x64-win32-llvm-debug cc config.config.load=tools/build-configs/windows-llvm-debug.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @x64-win32-llvm-release cc config.config.load=tools/build-configs/windows-llvm-release.config" -Wait
 	}
 }
 elseif ($ARCH -eq "ARM64" -or $ARCH -eq "ARM") 
 {
 	& {
 		Write-Output "ARM detected" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-msvc-debug cc config.config.load=tools/build_configs/windows-msvc-debug.config" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-llvm-debug cc config.config.load=tools/build_configs/windows-llvm-debug.config" -Wait
-		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-llvm-release cc config.config.load=tools/build_configs/windows-llvm-release.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-msvc-debug cc config.config.load=tools/build-configs/windows-msvc-debug.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-llvm-debug cc config.config.load=tools/build-configs/windows-llvm-debug.config" -Wait
+		Start-Process "bdep" -ArgumentList "init -C @arm64-win32-llvm-release cc config.config.load=tools/build-configs/windows-llvm-release.config" -Wait
 	}
 }
 else
