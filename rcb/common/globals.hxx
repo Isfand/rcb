fddf8bfaefec07154f34a71c4d9e7460ee3554f9
Isfand <20780130+Isfand@users.noreply.github.com>
2025-02-24 02:10:17 +0000

    Updated version to v0.12.0

--- rcb/common/globals.hxx
+++ rcb/common/globals.hxx
@@ -8,7 +8,7 @@ namespace rcb{
 
 //Project
 inline const char* g_progName{"rcb"};
-inline const char* g_progVersion{"0.11.0"};
+inline const char* g_progVersion{"0.12.0"};
 
 //Singleton
 inline Singleton* g_singleton = Singleton::instance();
