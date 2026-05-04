dbaec729fb7cf789a3b49aded6cdc07cd76ee162
Isfand <20780130+Isfand@users.noreply.github.com>
2026-05-04 05:06:55 +0100

    Turned globals into constexpr

--- rcb/rcb/common/globals.hxx
+++ rcb/rcb/common/globals.hxx
@@ -7,26 +7,26 @@
 namespace rcb{
 
 //Project
-inline const char* g_kProgName{"rcb"};
-inline const char* g_kProgVersion{"0.13.0"};
+inline constexpr const char* g_kProgName{"rcb"};
+inline constexpr const char* g_kProgVersion{"0.13.0"};
 
 //Env: Currently only used in env.cxx
-inline const char* g_kEnvFile {"file"};
-inline const char* g_kEnvData {"data"};
-inline const char* g_kEnvWipe {"wipe"};
-inline const char* g_kEnvSign {"sign"};
+inline constexpr const char* g_kEnvFile {"file"};
+inline constexpr const char* g_kEnvData {"data"};
+inline constexpr const char* g_kEnvWipe {"wipe"};
+inline constexpr const char* g_kEnvSign {"sign"};
 
 //Database: WARNING Do NOT change these. Doing so will break compatibility with existing tables with previous schema.
 inline const std::string g_kDatabaseName {std::string(g_kProgName) + ".sqlite3"}; //{"rcb.sqlite3"}; //Database filename
-inline const char* g_kSchemaID           {"id"};            //SQL auto increment
-inline const char* g_kSchemaFile         {"file"};          //Refers to filename to be inside of .rcb/file/
-inline const char* g_kSchemaPath         {"path"};          //Complete filesystem path
-inline const char* g_kSchemaTimestamp    {"timestamp"};     //Unix Timestamp since epoch
-inline const char* g_kSchemaSize         {"size"};          //Bytes
-inline const char* g_kSchemaFiletype     {"filetype"};      //Filesystem file type
-inline const char* g_kSchemaPathDepth    {"depth"};         //Path depth
-inline const char* g_kSchemaUser         {"user"};          //The user who executed the command
-inline const char* g_kSchemaExecution    {"execution"};     //Execution id of all files deleted in the same run
+inline constexpr const char* g_kSchemaID           {"id"};            //SQL auto increment
+inline constexpr const char* g_kSchemaFile         {"file"};          //Refers to filename to be inside of .rcb/file/
+inline constexpr const char* g_kSchemaPath         {"path"};          //Complete filesystem path
+inline constexpr const char* g_kSchemaTimestamp    {"timestamp"};     //Unix Timestamp since epoch
+inline constexpr const char* g_kSchemaSize         {"size"};          //Bytes
+inline constexpr const char* g_kSchemaFiletype     {"filetype"};      //Filesystem file type
+inline constexpr const char* g_kSchemaPathDepth    {"depth"};         //Path depth
+inline constexpr const char* g_kSchemaUser         {"user"};          //The user who executed the command
+inline constexpr const char* g_kSchemaExecution    {"execution"};     //Execution id of all files deleted in the same run
 
 //Singleton
 inline Singleton* g_singleton = Singleton::instance();
