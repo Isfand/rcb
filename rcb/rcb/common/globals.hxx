660ff5f9364a3a2635577e03d3842db2fcac84b2
Isfand <20780130+Isfand@users.noreply.github.com>
2026-05-14 12:14:24 +0100

    change: Updated CHANGELOG.txt

--- rcb/rcb/common/globals.hxx
+++ rcb/rcb/common/globals.hxx
@@ -8,7 +8,7 @@ namespace rcb{
 
 //Project
 inline constexpr const char* g_kProgName        {"rcb"};
-inline constexpr const char* g_kProgVersion     {"0.13.0"};
+inline constexpr const char* g_kProgVersion     {"0.14.0"};
 
 //Env: Currently only used in env.cxx
 inline constexpr const char* g_kEnvFile         {"file"};
