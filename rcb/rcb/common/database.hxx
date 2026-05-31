bb39b90f2497670341284ec0343c201b80c44449
Isfand <20780130+Isfand@users.noreply.github.com>
2026-06-01 00:48:54 +0100

    add: Add DTO struct
    New struct to be used as a Data Transfer Object for the database. This will eventually replace the relevant globals.

--- rcb/rcb/common/database.hxx
+++ rcb/rcb/common/database.hxx
@@ -6,8 +6,40 @@
 
 #include <string>
 #include <vector>
+#include <optional>
+#include <filesystem>
 
 namespace rcb{
+
+// Data Transfer Object.
+struct DTO
+{
+	struct Meta
+	{
+		static constexpr const char* kDatabaseName    {"rcb.sqlite3"}; // Database filename
+		static constexpr const char* kTableName       {"rcb"};         // table name
+		
+		static constexpr const char* kSchemaID        {"id"};          // SQL auto increment
+		static constexpr const char* kSchemaFile      {"file"};        // Refers to filename to be inside of .rcb/file/
+		static constexpr const char* kSchemaPath      {"path"};        // Complete filesystem path
+		static constexpr const char* kSchemaTimestamp {"timestamp"};   // Unix Timestamp since epoch
+		static constexpr const char* kSchemaSize      {"size"};        // Bytes
+		static constexpr const char* kSchemaFiletype  {"filetype"};    // Filesystem file type
+		static constexpr const char* kSchemaPathDepth {"depth"};       // Path depth
+		static constexpr const char* kSchemaUser      {"user"};        // The user who executed the command
+		static constexpr const char* kSchemaExecution {"execution"};   // Execution id of all files deleted in the same run	
+	};
+
+	std::optional<unsigned long long int> id;
+	std::optional<std::string> file;
+	std::optional<std::filesystem::path> path;
+	std::optional<long long int> timestamp;
+	std::optional<unsigned long long int> size;
+	std::optional<std::string> string;
+	std::optional<unsigned long long int> depth;
+	std::optional<std::string> user;
+	std::optional<unsigned long long int> execution;
+};
 
 class Database
 {
