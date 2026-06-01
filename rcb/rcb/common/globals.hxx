6dce053d451c386e51103625abeba56e71bd3643
Isfand <20780130+Isfand@users.noreply.github.com>
2026-06-01 15:32:01 +0100

    style: Remove whitespace

--- rcb/rcb/common/globals.hxx
+++ rcb/rcb/common/globals.hxx
@@ -5,8 +5,8 @@
 namespace rcb{
 
 // Project
-inline constexpr const char* g_kProgName        {"rcb"};
-inline constexpr const char* g_kProgVersion     {"0.15.0"};
+inline constexpr const char* g_kProgName    {"rcb"};
+inline constexpr const char* g_kProgVersion {"0.15.0"};
 
 // Singleton
 inline Singleton* g_singleton = Singleton::instance();
